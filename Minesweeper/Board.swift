95d34270ad8fd68ce477d9d1de4cb54629f6af9a
Matt Le Fleur <mlefleur@googlemail.com>
2017-06-25 15:59:17 +0100

    Wrote board initialiser for size

--- Minesweeper/Board.swift
+++ Minesweeper/Board.swift
@@ -12,6 +12,19 @@ class Board  {
     var squares: [[Int]] = [] // 2d array to represent bombs
     
     init(size: Int, bombs: String) {
+        // initialise array with 0s (no bomb)
         
+        // Rows
+        let row: [Int] = []
+        for _ in 0..<size {
+            self.squares.append(row)
+        }
+        
+        // Columns
+        for x in 0..<size {
+            for _ in 0..<size {
+                self.squares[x].append(0)
+            }
+        }
     }
 }
