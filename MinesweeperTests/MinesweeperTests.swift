55657588591c9ba9c801ac70e356cf9ec512c99c
Matt Le Fleur <mlefleur@googlemail.com>
2017-06-25 16:11:14 +0100

    New tests for bomb positioning

--- MinesweeperTests/MinesweeperTests.swift
+++ MinesweeperTests/MinesweeperTests.swift
@@ -13,11 +13,31 @@ class MinesweeperTests: XCTestCase {
     
     var board: Board!
     let size = 7
+    let bombJSON = "[[0,0],[0,4],[1,1],[1,4],[1,5],[2,0],[2,2],[2,5],[3,1],[3,4],[3,5],[4,4],[4,5],[5,0],[5,3],[5,6],[6,0],[6,2],[6,5]]"
+    
+    /*
+     +---+---+---+---+---+---+---+
+     | X |   | X |   |   | X | X |
+     +---+---+---+---+---+---+---+
+     |   | X |   | X |   |   |   |
+     +---+---+---+---+---+---+---+
+     |   |   | X |   |   |   | X |
+     +---+---+---+---+---+---+---+
+     |   |   |   |   |   | X |   |
+     +---+---+---+---+---+---+---+
+     | X | X |   | X | X |   |   |
+     +---+---+---+---+---+---+---+
+     |   | X | X | X | X |   | X |
+     +---+---+---+---+---+---+---+
+     |   |   |   |   |   | X |   |
+     +---+---+---+---+---+---+---+
+     */
+    
     
     override func setUp() {
         super.setUp()
         
-        self.board = Board(size: self.size, bombs: "")
+        self.board = Board(size: self.size, bombs: bombJSON)
     }
     
     override func tearDown() {
@@ -53,6 +73,15 @@ class MinesweeperTests: XCTestCase {
         }
         
         XCTAssert(counter == self.size * self.size) // eg size = 7, therefore counter = 49
+    }
+    
+    func testBombs() {
+        // JSON string should insert bombs into correct positions
+        // based on above json - 0,0 should have bomb
+        XCTAssert(self.board.squares[0][0] == 1)
+        
+        // base on above json - 1,0 should have no bomb
+        XCTAssert(self.board.squares[1][0] == 0)
     }
     
 }
